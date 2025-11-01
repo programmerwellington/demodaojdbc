97cae0231203046b22abda3f6aeea0e309de05d9
wnetsocials <167631248+wnetsocials@users.noreply.github.com>
2025-11-01 12:21:02 -0300

    SellerDaoJDBC and DaoFActory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,15 @@ package application;
 import model.entities.Department;
 import model.entities.Seller;
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 public class Program {
 	public static void main (String[] args) {
 		Department obj = new Department(1, "Electronics");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000., obj);
 		System.out.println(seller);
+		
+		SellerDao sd = DaoFactory.createSellerDao();
 		
 	}
 }
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,7 @@
+package model.dao;
+import model.dao.impl.SellerDaoImplJDBC;
+public class DaoFactory {
+	public static SellerDao createSellerDao() {
+		return new SellerDaoImplJDBC();
+	}
+}
