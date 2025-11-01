19d9d83a86afe611c1933e0e9ab69393b03cd979
wnetsocials <167631248+wnetsocials@users.noreply.github.com>
2025-11-01 11:09:50 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,8 @@
+package application;
+import model.entities.Department;
+public class Program {
+	public static void main (String[] args) {
+		Department obj = new Department(1, "depto");
+		System.out.println(obj);
+	}
+}
