cfe9b168e5853000578adb9b11cf9fcbfb4eb95b
wnetsocials <167631248+wnetsocials@users.noreply.github.com>
2025-11-05 15:15:19 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,25 +4,35 @@ import model.entities.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import java.util.List;
+import java.util.Date;
 public class Program {
 	public static void main (String[] args) {
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = sellerDao.findById(3);
 		System.out.println("********** TEST 1 => seller findById **********\n" + seller + "\n");
 		
+		System.out.println("\n********** TEST 2 => seller findByDepartment **********");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		System.out.println("\n********** TEST 2 => seller findByDepartment **********");
 		for (Seller s : list) {
 			System.out.println(s);
 		}
 		
-		List<Seller> listAll = sellerDao.findAll();
 		System.out.println("\n********** TEST 3 => seller findAll **********");
+		List<Seller> listAll = sellerDao.findAll();
 		for (Seller sAll : listAll) {
 			System.out.println(sAll);
 		}
+		
+		System.out.println("\n********** TEST 4 => seller insert **********");
+		Seller newSeller = new Seller(null, "Liliana D", "liliana@gmail.com", new Date(), 4000., department);
+		sellerDao.insert(newSeller);
+		System.out.println("New Seller inserted: " + newSeller.getId());
+
+		
+		// System.out.println(DaoFactory.createSellerDao().findById(8));
 		
 	}
 }
--- src/model/dao/impl/SellerDaoImplJDBC.java
+++ src/model/dao/impl/SellerDaoImplJDBC.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import db.DB;
 import db.DbException;
 import model.entities.Department;
@@ -22,7 +23,39 @@ public class SellerDaoImplJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"insert into seller " +
+					"(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+					"values " +
+					"(?, ?, ?, ?, ?) ",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0 ) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -49,7 +82,7 @@ public class SellerDaoImplJDBC implements SellerDao {
 					"inner join department " +
 					"on seller.DepartmentId = department.Id " +
 					"where seller.Id = ?");
-			st.setInt(1, 3);
+			st.setInt(1, id);
 			rs = st.executeQuery();
 			
 			if (rs.next()) {
@@ -61,8 +94,7 @@ public class SellerDaoImplJDBC implements SellerDao {
 			
 		} catch(SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
@@ -99,8 +131,7 @@ public class SellerDaoImplJDBC implements SellerDao {
 			
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
@@ -140,8 +171,7 @@ public class SellerDaoImplJDBC implements SellerDao {
 			
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
